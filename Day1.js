a8359b61a19b2c179c9be0d292aec9617e1104ca
DeluluDreamer <karthick32456@gmail.com>
2026-08-25 21:11:38 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,15 @@
+let studentName = "Arun";
+let age = 20;
+let mark = 85;
+console.log(studentName);
+console.log(age);
+console.log(mark);
+
+let name1= prompt("Enter your name");
+alert(name1);
+let age2 = prompt("Enter your age");
+console.log(age2);
+let answer = confirm("Do you like JavaScript?");
+console.log(answer);
+let name3 = prompt("Enter your name");
+document.writeln(name3);
